0579404c368d5818fb219aadea27c5a25235372e
miciek <miciek@gmail.com>
2018-12-06 23:36:11 +0100

    Update examples and add a new one

---
+++ src/main/scala/SequencedNestedFlatMaps.scala
@@ -0,0 +1,14 @@
+class SequencedNestedFlatMaps extends App {
+  val s = List(1, 2, 3)
+    .flatMap(a => List(a * 2))
+    .flatMap(b => List(b, b + 10))
+  assert(s == List(2, 12, 4, 14, 6, 16))
+
+  val n = List(1, 2, 3)
+    .flatMap(a => {
+      List(a * 2)
+        .flatMap(b => List(b, b + 10))
+    })
+  println(s)
+  assert(s == n)
+}
