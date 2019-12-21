b0d31bc81c0244c14921003a0d4f7a4006657003
miciek <miciek@gmail.com>
2019-12-21 17:34:41 +0100

    Add case class examples for chapter 4

---
+++ src/main/scala/ProgrammingLanguages.scala
@@ -0,0 +1,22 @@
+object ProgrammingLanguages {
+  case class ProgrammingLanguage(name: String, year: Int)
+
+  val java  = ProgrammingLanguage("Java", 1995)
+  val scala = ProgrammingLanguage("Scala", 2004)
+
+  println(java.name)
+
+  println(scala.name.length)
+
+  println((scala.year + java.year) / 2)
+
+  val languages = List(java, scala)
+
+  println(languages)
+
+  val names = languages.map(lang => lang.name)
+  println(names)
+
+  val young = languages.filter(lang => lang.year > 2000)
+  println(young)
+}
