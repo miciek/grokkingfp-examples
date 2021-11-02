df3b7bec643053cb716eccd4db8134dc0a5a44ea
miciek <miciek@gmail.com>
2021-11-02 17:34:22 +0100

    Add assertions to ch04 programming language examples

--- src/main/scala/ch04_ProgrammingLanguages.scala
+++ src/main/scala/ch04_ProgrammingLanguages.scala
@@ -1,22 +1,32 @@
 object ch04_ProgrammingLanguages extends App {
   case class ProgrammingLanguage(name: String, year: Int)
 
-  val java  = ProgrammingLanguage("Java", 1995)
-  val scala = ProgrammingLanguage("Scala", 2004)
+  val javalang  = ProgrammingLanguage("Java", 1995)
+  val scalalang = ProgrammingLanguage("Scala", 2004)
 
-  println(java.name)
+  assert(javalang.name == "Java")
+  println(javalang.name)
 
-  println(scala.name.length)
+  println(scalalang.name.length)
+  assert(scalalang.name.length == 5)
 
-  println((scala.year + java.year) / 2)
+  println((scalalang.year + javalang.year) / 2)
+  assert((scalalang.year + javalang.year) / 2 == 1999)
 
-  val languages = List(java, scala)
+  val languages = List(javalang, scalalang)
 
   println(languages)
+  assert(languages == List(ProgrammingLanguage("Java", 1995), ProgrammingLanguage("Scala", 2004)))
 
   val names = languages.map(lang => lang.name)
   println(names)
+  assert(names == List("Java", "Scala"))
 
   val young = languages.filter(lang => lang.year > 2000)
   println(young)
+  assert(young == List(scalalang))
+
+  assert(languages.map(_.name) == List("Java", "Scala"))
+
+  assert(languages.filter(_.year > 2000) == List(scalalang))
 }
