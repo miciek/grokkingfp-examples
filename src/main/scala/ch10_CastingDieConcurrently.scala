04aba5f4beb10419b3b830c06103d2f03e1d48a0
Michał Płachta <miciek@exul.net>
2025-01-17 14:15:46 +0100

    Switch to using raw main in ch10_CastingDieConcurrently

--- src/main/scala/ch10_CastingDieConcurrently.scala
+++ src/main/scala/ch10_CastingDieConcurrently.scala
@@ -6,7 +6,7 @@ import java.util.concurrent.{Executors, ScheduledThreadPoolExecutor}
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
 
-object ch10_CastingDieConcurrently extends App {
+object ch10_CastingDieConcurrently {
   import ch08_CastingDieImpure.NoFailures.castTheDieImpure
 
   def castTheDie(): IO[Int] = IO.delay(castTheDieImpure())
@@ -23,55 +23,58 @@ object ch10_CastingDieConcurrently extends App {
     result
   }
 
-  // Practicing refs and concurrent IOs:
-  // 1. wait 1 second, then cast two dies concurrently, wait for both of them and return their sum
-  unsafeRunTimedIO(for {
-    _      <- IO.sleep(1.second) // introduce 1.second
-    result <- List(castTheDie(), castTheDie()).parSequence
-  } yield result.sum) // or result.foldLeft(0)(_ + _)
+  def main(args: Array[String]): Unit = {
 
-  // 2. cast two dies concurrently, store each result in an atomic reference that holds a List, and return the final list as a result
-  unsafeRunTimedIO(for {
-    storedCasts <- Ref.of[IO, List[Int]](List.empty)
-    singleCast   = castTheDie().flatMap(result => storedCasts.update(_.appended(result)))
-    _           <- List(singleCast, singleCast).parSequence
-    casts       <- storedCasts.get
-  } yield casts)
+    // Practicing refs and concurrent IOs:
+    // 1. wait 1 second, then cast two dies concurrently, wait for both of them and return their sum
+    unsafeRunTimedIO(for {
+      _      <- IO.sleep(1.second) // introduce 1.second
+      result <- List(castTheDie(), castTheDie()).parSequence
+    } yield result.sum) // or result.foldLeft(0)(_ + _)
 
-  // 3. cast three dies concurrently, store each result in an atomic reference that holds a List, and return its value as a result
-  unsafeRunTimedIO(for {
-    storedCasts <- Ref.of[IO, List[Int]](List.empty)
-    singleCast   = castTheDie().flatMap(result => storedCasts.update(_.appended(result)))
-    _           <- List.fill(3)(singleCast).parSequence // introduce List.fill
-    casts       <- storedCasts.get
-  } yield casts)
+    // 2. cast two dies concurrently, store each result in an atomic reference that holds a List, and return the final list as a result
+    unsafeRunTimedIO(for {
+      storedCasts <- Ref.of[IO, List[Int]](List.empty)
+      singleCast   = castTheDie().flatMap(result => storedCasts.update(_.appended(result)))
+      _           <- List(singleCast, singleCast).parSequence
+      casts       <- storedCasts.get
+    } yield casts)
 
-  // 4. cast one hundred dies concurrently, store the total number of sixes in an atomic ref, and return its value as a result
-  unsafeRunTimedIO(for {
-    storedCasts <- Ref.of[IO, Int](0)
-    singleCast   = castTheDie().flatMap(result => if (result == 6) storedCasts.update(_ + 1) else IO.unit)
-    _           <- List.fill(100)(singleCast).parSequence
-    casts       <- storedCasts.get
-  } yield casts)
+    // 3. cast three dies concurrently, store each result in an atomic reference that holds a List, and return its value as a result
+    unsafeRunTimedIO(for {
+      storedCasts <- Ref.of[IO, List[Int]](List.empty)
+      singleCast   = castTheDie().flatMap(result => storedCasts.update(_.appended(result)))
+      _           <- List.fill(3)(singleCast).parSequence // introduce List.fill
+      casts       <- storedCasts.get
+    } yield casts)
 
-  // 5. cast one hundred dies concurrently, wait 1 second before each of them, and return their sum (without using an atomic ref)
-  unsafeRunTimedIO(List.fill(100)(IO.sleep(1.second).flatMap(_ => castTheDie())).parSequence.map(_.sum))
+    // 4. cast one hundred dies concurrently, store the total number of sixes in an atomic ref, and return its value as a result
+    unsafeRunTimedIO(for {
+      storedCasts <- Ref.of[IO, Int](0)
+      singleCast   = castTheDie().flatMap(result => if (result == 6) storedCasts.update(_ + 1) else IO.unit)
+      _           <- List.fill(100)(singleCast).parSequence
+      casts       <- storedCasts.get
+    } yield casts)
 
-  // BONUS content (not in the book)
-  { // IO.delay vs IO.sleep
-    // let's create IORuntime that has a single thread
-    val runtime = singleThreadRuntime()
+    // 5. cast one hundred dies concurrently, wait 1 second before each of them, and return their sum (without using an atomic ref)
+    unsafeRunTimedIO(List.fill(100)(IO.sleep(1.second).flatMap(_ => castTheDie())).parSequence.map(_.sum))
 
-    // we now have two different versions of sleeping, one puts the thread to sleep,
-    // while the second one puts the fiber to sleep.
-    val threadSleep = IO.delay(Thread.sleep(1000))
-    val fiberSleep  = IO.sleep(1.second)
+    // BONUS content (not in the book)
+    { // IO.delay vs IO.sleep
+      // let's create IORuntime that has a single thread
+      val runtime = singleThreadRuntime()
 
-    println("Thread.sleep concurrently in 3 fibers on a single thread:")
-    unsafeRunTimedIO(List.fill(3)(threadSleep).parSequence)(runtime)
+      // we now have two different versions of sleeping, one puts the thread to sleep,
+      // while the second one puts the fiber to sleep.
+      val threadSleep = IO.delay(Thread.sleep(1000))
+      val fiberSleep  = IO.sleep(1.second)
 
-    println("IO.sleep concurrently in 3 fibers on a single thread:")
-    unsafeRunTimedIO(List.fill(3)(fiberSleep).parSequence)(runtime)
+      println("Thread.sleep concurrently in 3 fibers on a single thread:")
+      unsafeRunTimedIO(List.fill(3)(threadSleep).parSequence)(runtime)
+
+      println("IO.sleep concurrently in 3 fibers on a single thread:")
+      unsafeRunTimedIO(List.fill(3)(fiberSleep).parSequence)(runtime)
+    }
   }
 
   private def singleThreadRuntime(): IORuntime = {
